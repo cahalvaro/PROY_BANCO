19d29aa41c96a57b83d01b2d0f9baae6aabbc40b
Alvaro <caalvaro123@gmail.com>
2024-11-02 22:51:30 -0400

    Transferencias actualizado

--- data/transferencia.py
+++ data/transferencia.py
@@ -19,3 +19,15 @@ class TransferenciaData():
         except Exception as ex:
             print("Tabla transferencias OK")
 
+    def registrar(self, info:Transferencia):
+        fecha=datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        self.db=con.Conexion().conectar()
+        self.cursor=self.db.cursor()
+        self.cursor.execute("""
+        INSERT INTO transferencias values(null, '{}','{}','{}','{}','{}','{}','{}','{}')
+        """.format(info._monto, info._tipo, info._documento, info._internacional, info._dolares,fecha,False,info._motivo))
+        self.cursor.commit()
+        if self.cursor.rowcount==1:
+            return True
+        else:
+            return False
