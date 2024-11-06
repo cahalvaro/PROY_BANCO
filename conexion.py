ef17da0fcfeca3176a8c81c77c188077ec43f71e
Alvaro <caalvaro123@gmail.com>
2024-11-05 22:16:36 -0400

    Modificaciones

--- conexion.py
+++ conexion.py
@@ -16,36 +16,15 @@ class Conexion():
         cur.close()
         self.crearAdmin()
         
-///
-class Conexion():
-    def __init__(self):
-        try :
-            self.con=sqlite3.connect("banco.db")
-            self.crearTablas()
+     #primer commit hasta aqui
+    def crearAdmin(self):
+        try:
+            sql_insert="""INSERT INTO usuarios values(null, '{}', '{}', '{}')""".format("Administrador", "admin","admin2050.")
+            cur=self.con.cursor()
+            cur.execute(sql_insert)
+            self.con.commit()
         except Exception as ex:
-            print(ex)
+            print("Ya se creo el usuario admin", ex)
 
-    def crearTablas(self):
-        sql_create_table1="""CREATE TABLE IF NOT EXISTS usuarios(id INTEGER PRIMARY KEY 
-                            AUTOINCREMENT, nombre TEXT, usuario TEXT UNIQUE , clave TEXT)"""
-        cur=self.con.cursor()
-        cur.execute(sql_create_table1)
-        cur.close()
-        self.crearAdmin()
-        class Conexion():
-    def __init__(self):
-        try :
-            self.con=sqlite3.connect("banco.db")
-            self.crearTablas()
-        except Exception as ex:
-            print(ex)
-
-    def crearTablas(self):
-        sql_create_table1="""CREATE TABLE IF NOT EXISTS usuarios(id INTEGER PRIMARY KEY 
-                            AUTOINCREMENT, nombre TEXT, usuario TEXT UNIQUE , clave TEXT)"""
-        cur=self.con.cursor()
-        cur.execute(sql_create_table1)
-        cur.close()
-        self.crearAdmin()
-
-
+    def conectar(self):
+        return self.con
