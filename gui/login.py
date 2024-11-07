41c1f4100554aee4ba0f51d565fbe652d4176024
Alvaro <caalvaro123@gmail.com>
2024-11-06 22:17:34 -0400

    Actualizaciones main y login

--- gui/login.py
+++ gui/login.py
@@ -35,3 +35,26 @@ class Login():
 
     def initGUI(self):
         self.login.btnAcceder.clicked.connect(self.ingresar)
+        ///////
+
+    def ingresar(self):
+        if len(self.login.txtUsuario.text()) < 2:
+            self.login.lblMensaje.setText("Ingrese un usuario valido")
+            self.login.txtUsuario.setFocus()
+        elif len(self.login.txtClave.text()) < 3:
+            self.login.lblMensaje.setText("Ingrese una contraseña valida")
+            self.login.txtClave.setFocus()
+        else:
+            self.login.lblMensaje.setText("")
+            usu=Usuario(usuario=self.login.txtUsuario.text(),clave=self.login.txtClave.text() )
+            usuData=UsuarioData()
+            res=usuData.login(usu)
+            if res:
+                self.main=MainWindow()
+                self.login.hide()
+            else:
+                self.login.lblMensaje.setText("Datos de acceso incorrectos")
+
+
+    def initGUI(self):
+        self.login.btnAcceder.clicked.connect(self.ingresar)
--- gui/main.py
+++ gui/main.py
@@ -51,3 +51,53 @@ class MainWindow():
                 dolares=self.registro.checkDolares.isChecked(), 
                 internacional=self.registro.checkInternacional.isChecked()
             )
+
+
+/////
+class MainWindow():
+    def __init__(self):
+        self.main=uic.loadUi("gui/main.ui")
+        self.initGUI()
+        self.main.showMaximized()
+
+    
+    def initGUI(self):
+        self.main.btnRegistrarTransferencias.triggered.connect(self.abrirRegistro)
+        self.registro=uic.loadUi("gui/registroTransacciones.ui")
+
+    def abrirRegistro(self):
+        self.registro.btnRegistrar.clicked.connect(self.registrarTransaccion)
+        self.registro.show()
+        
+
+    def registrarTransaccion(self):
+        if self.registro.cbTipo.currentText()=="***Seleccione una opcion":
+            mBox=QMessageBox()
+            mBox.setText("Debe seleccionar un tipo de documento")
+            mBox.exec()
+            self.registro.cbTipo.setFocus()
+        elif len(self.registro.txtDocumento.text())<4:
+            mBox=QMessageBox()
+            mBox.setText("Debe ingresar un documento valido")
+            mBox.exec()
+            self.registro.txtDocumento.setFocus()
+        elif self.registro.cbMotivo.currentText()=="***Seleccione una opcion":
+            mBox=QMessageBox()
+            mBox.setText("Debe seleccionar el motivo")
+            mBox.exec()
+            self.registro.cbMotivo.setFocus()
+        elif not self.registro.txtMonto.text().isnumeric():
+            mBox=QMessageBox()
+            mBox.setText("Debe ingresar un monto valido")
+            mBox.exec()
+            self.registro.txtMonto.setText("0")
+            self.registro.txtMonto.setFocus()
+        else:
+            transferencia=Transferencia(
+                tipo=self.registro.cbTipo.currentText(),
+                documento=self.registro.txtDocumento.text(),
+                monto=float(self.registro.txtMonto.text()),
+                motivo=self.registro.cbMotivo.currentText(),
+                dolares=self.registro.checkDolares.isChecked(), 
+                internacional=self.registro.checkInternacional.isChecked()
+            )
