8af0de0670b4f2b52943b27db55aa2b189fe0bcd
Alvaro <caalvaro123@gmail.com>
2024-11-15 23:12:32 -0400

    Actualizaciones

--- data/transferencia.py
+++ data/transferencia.py
@@ -6,44 +6,41 @@ class TransferenciaData():
 
     def __init__(self):
         try:
-            self.db=con.Conexion().conectar()
-            self.cursor=self.db.cursor()
-            sql_create_transferencias="""CREATE TABLE IF NOT EXISTS transferencias
+            self.db = con.Conexion().conectar()
+            self.cursor = self.db.cursor()
+            sql_create_transferencias = """ CREATE TABLE IF NOT EXISTS transferencias 
             (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            monto NUMERIC, tipo TEXT, documento TEXT,internacional BOOLEAN,
+            monto NUMERIC,
+            tipo TEXT, 
+            documento TEXT, 
+            internacional BOOLEAN, 
             dolares BOOLEAN, 
-            fecha_registro DATETIME , verificado BOOLEAN, motivo TEXT)"""
+            fecha_registro DATETIME, 
+            verificado BOOLEAN, 
+            motivo TEXT)"""
+            
             self.cursor.execute(sql_create_transferencias)
             self.db.commit()
             self.cursor.close()
             self.db.close()
             print("Tabla transferencias Creada")
         except Exception as ex:
-            print("Tabla transferencias OK",ex)
+            print("Tabla transferencias OK: ",ex)
 
-    def registrar(self, info:Transferencia):
-        fecha=datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        self.db=con.Conexion().conectar()
-        self.cursor=self.db.cursor()
+    def registrar(self,info:Transferencia):
+        fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.db = con.Conexion().conectar()
+        self.cursor = self.db.cursor()
         self.cursor.execute("""
         INSERT INTO transferencias values(null,'{}','{}','{}','{}','{}','{}','{}','{}')
-        """.format(info._monto, info._tipo, info._documento, info._internacional, info._dolares,fecha,False,info._motivo))
+        """.format(info._monto,info._tipo,info._documento,info._internacional,info._dolares,fecha,False,info._motivo))
         self.db.commit()
         if self.cursor.rowcount==1:
             return True
         else:
             return False
+        
+            
 
 
-    ////    def registrar(self, info:Transferencia):
-        fecha=datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        self.db=con.Conexion().conectar()
-        self.cursor=self.db.cursor()
-        self.cursor.execute("""
-        INSERT INTO transferencias values(null,'{}','{}','{}','{}','{}','{}','{}','{}')
-        """.format(info._monto, info._tipo, info._documento, info._internacional, info._dolares,fecha,False,info._motivo))
-        self.db.commit()
-        if self.cursor.rowcount==1:
-            return True
-        else:
-            return False
+    
--- gui/registro.py
+++ gui/registro.py
@@ -1,9 +1,6 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QMessageBox
 
-
-
-
 class RegistroWindow():
     def __init__(self):
         self.v = uic.loadUi("gui/registroTransacciones.ui")
